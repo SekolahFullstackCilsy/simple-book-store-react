b2d071e36ef2cf5b482ee403d3260e523fdbdfe2
RizkyArifNur <rizkyarifnur@gmail.com>
2021-01-21 23:54:02 +0700

    attach CRUD pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route, Link, BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import CreateBook from "./Page/CreateBookPage";
+import BookDetail from "./Page/BookDetailPage";
+import BooksList from "./Page/BookListPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <nav className="navbar navbar-expand navbar-dark bg-dark">
+            <Link to={"/books"} className="navbar-brand">
+              Sekolah Fullstack Bookstore
+            </Link>
+            <div className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to={"/books"} className="nav-link">
+                  Books
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to={"/books/add"} className="nav-link">
+                  Add
+                </Link>
+              </li>
+            </div>
+          </nav>
+
+          <div className="container mt-3">
+            <Switch>
+              <Route exact path="/books" component={BooksList} />
+              <Route exact path="/books/add" component={CreateBook} />
+              <Route path="/books/:id" component={BookDetail} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
